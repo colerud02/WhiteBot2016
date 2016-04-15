1fbc33f4d77b2a2a6a609e05ea9a2336f7742028
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-14 18:28:57 -0700

    test

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,5 +1,7 @@
 package org.jointheleague.iaroc;
 
+import android.os.SystemClock;
+
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
@@ -9,7 +11,7 @@ import org.jointheleague.iaroc.sensors.UltraSonicSensors;
 public class Brain extends IRobotCreateAdapter {
     private final Dashboard dashboard;
     public UltraSonicSensors sonar;
-
+    int distance = 0;
     public Brain(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
             throws ConnectionLostException {
         super(create);
@@ -27,5 +29,24 @@ public class Brain extends IRobotCreateAdapter {
 
     }
     /* This method is called repeatedly. */
-    public void loop() throws ConnectionLostException {}
+    public void loop() throws ConnectionLostException {
+        for (int i = 1; i < 66666; i++){
+            this.driveDirect(i/9, 500);
+            SystemClock.sleep(1000);
+            this.driveDirect(i * 9, 500);
+            SystemClock.sleep(1000);
+        }
+        readSensors(0);
+
+        readSensors(0);
+        distance = getDistance();
+        if (isBumpRight()==true||isBumpLeft()==true){
+            dashboard.speak("Screw that!");
+            this.driveDirect(200, -200);
+            SystemClock.sleep(3000);
+        }
+
+        dashboard.log(""+distance);
+
+    }
 }
